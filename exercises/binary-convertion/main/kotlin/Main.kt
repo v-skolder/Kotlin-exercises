04e12446d1b3c775236014e01cb02474a69c2041
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-06-26 13:10:20 -0300

    feat: add new exercise (without resolution)

--- exercises/binary-convertion/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val input = readln()
-
-    val slice = input.split(",")
-
-    val validNumbers = slice.filter { it.toInt(2) % 5 == 0 && it.toInt(2) != 0 }
-
-    println(validNumbers.joinToString(","))
-}
